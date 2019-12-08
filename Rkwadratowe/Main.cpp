5483b97a6ce8b7fe6419da294003ca7c0efd9c3e
With3rr <45690230+With3rr@users.noreply.github.com>
2019-12-08 15:37:06 +0100

    Create Main.cpp

---
+++ Rkwadratowe/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Rkwadratowe.h"
+using namespace std;
+
+int main()
+{
+	Rkwadratowe* s1 = new Rkwadratowe;
+	s1->zamien(1, 5, 6);
+	s1->wypisz_pierwiastki();
+
+
+	/*string a = "123";
+	int s1 = stoi(a);
+	cout << s1+20;*/
+
+    
+}
