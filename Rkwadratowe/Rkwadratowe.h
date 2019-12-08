75c7167b537faba8f17d4cab046ff682e12b7b9e
With3rr <45690230+With3rr@users.noreply.github.com>
2019-12-08 15:37:40 +0100

    Create Rkwadratowe.h

---
+++ Rkwadratowe/Rkwadratowe.h
@@ -0,0 +1,11 @@
+class Rkwadratowe
+{
+private:
+	float a, b, c, delta, x1, x2;
+public:
+	void zamien(float, float ,float);
+	void oblicz(void);
+	void wypisz_pierwiastki(void);
+	float zwroc_delta(void);
+
+};
